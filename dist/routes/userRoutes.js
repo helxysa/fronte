76b48fc052a96d00bab4a62d60cd26f074788b25
PColares <siqueira.pedro1998@gmail.com>
2024-06-25 13:22:36 -0300

    removendo backend sequelize

--- dist/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/', userController.getUsers);
-
-module.exports = router;
