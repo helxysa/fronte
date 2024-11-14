d2dfd2b9123d7bd164e9b20d8faf8ed795544bd6
PColares <siqueira.pedro1998@gmail.com>
2024-11-14 15:14:43 -0300

    correção da configuração do websocket

--- start/ws.ts
+++ start/ws.ts
@@ -5,10 +5,10 @@ app.ready(() => {
   ws.boot()
   const io = ws.io
   io?.on('connection', (socket) => {
-    console.log('A new connection', socket.id)
+    // console.log('A new connection', socket.id)
 
     socket.on('medicao:update', (data) => {
-      console.log('Evento medicao:update recebido do cliente:', data)
+      // console.log('Evento medicao:update recebido do cliente:', data)
 
       // Reemitir o evento para todos os clientes conectados
       io.emit('medicao:update', data)
